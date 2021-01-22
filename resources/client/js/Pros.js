b4af5438ce1e62331c0aab903f0d79635361cefb
navid <93487@farnborough.ac.uk>
2021-01-22 00:01:00 +0000

    Finished the pro.html page

--- resources/client/js/Pros.js
+++ resources/client/js/Pros.js
@@ -2,7 +2,7 @@
 function getProsList() {
     debugger;
     console.log("Invoked getProsList()");     //console.log your BFF for debugging client side - also use debugger statement
-    const url = "/Pros/list/";    		// API method on web server will be in Pros class, method list
+    const url = "/pros/proslist/";    		// API method on web server will be in Pros class, method list
     fetch(url, {
         method: "GET",				//Get method
     }).then(response => {
@@ -11,15 +11,50 @@ function getProsList() {
         if (response.hasOwnProperty("Error")) { //checks if response from the web server has an "Error"
             alert(JSON.stringify(response));    // if it does, convert JSON object to string and alert (pop up window)
         } else {
-            formatProsList(response);          //this function will create an HTML table of the data (as per previous lesson)
+            formatProsList(response);          //this function will create an HTML table of the data
         }
     });
 }
 
-function formatProsList(myJSONArray){
-    let dataHTML = "";
-    for (let item of myJSONArray) {
-        dataHTML += "<tr><td>" + item.ProID + "<td><td>" + item.ProName + "<tr><td>";
+function formatProsList(myJSONArray) {
+    if (myJSONArray[0] == undefined) { //if the ProName does not exist in the case of inputting non existant names
+        alert("Error - ProName does not exist or is written with incorrect case"); //error for debugging
+    } else {
+
+        let dataHTML = "<tr><th>Name</th><th>Position</th><th>Average Kills</th><th>Average Deaths</th><th>Average Assists</th><th>KDA</th></tr>"; //table headings for clarity
+        for (let item of myJSONArray) {
+            dataHTML += "<tr>" + "<td>" + item.ProName + "</td>" + "<td>" + item.Position + "</td>" + "<td>" + item.AverageKills + "</td>" + "<td>" + item.AverageDeaths + "</td>" + "<td>" + item.AverageAssists + "</td>" + "<td>" + item.KDA + "</td>" + "</tr>";//formatting of the table
+        }
+        if (myJSONArray.length !== 1){ //checks the length of the array, if there is only 1 player it will edit the Pro table, otherwise it will edit the Pros table
+            document.getElementById("ProsTable").innerHTML = dataHTML;
+        }else{
+            document.getElementById("ProTable").innerHTML = dataHTML;
+        }//links to the table in pros.html
     }
-    document.getElementById("ProsTable").innerHTML = dataHTML;
+}
+function getPros(ProName) { //fetches the a particular pro
+    debugger;
+    console.log("Invoked getPros()");     //console.log for debugging client side - also uses debugger statement
+    const url = "/pros/getPro/";    		// API method on web server will be in Pros class
+    fetch(url + ProName, {
+        method: "GET",				//GET method
+    }).then(response => {
+        return response.json();                 //return response as JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) { //checks if response from the web server has an "Error"
+            alert(JSON.stringify(response));// if it does, convert JSON object to string and alert (pop up window)
+        } else {
+            formatProsList(response);          //this function will create an HTML table of the data but in this case a single record
+        }
+    });
+}
+function valPros(){  //function for validating user inputs for ProName
+    debugger;
+    console.log("Invoked Pros()"); //invoke for debugging
+    let formData = new FormData(document.getElementById('ProsForm')) // links to the form on the Pros.html page
+    if (formData.get('ProName').length < 2 || formData.get('ProName').length >15){
+        alert("Error - ProName must be within the 2-15 character boundary"); //error for debugging
+    }else {
+        getPros(formData.get('ProName'))//calls getPros
+    }
 }
--- src/main/java/controllers/Pros.java
+++ src/main/java/controllers/Pros.java
@@ -4,24 +4,31 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import server.Main;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+@Path("pros/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
 
 public class Pros {
     @GET
-    @Path("Prolist")
-    public String UsersList() {
+    @Path("proslist")
+    public String prosList() {
         System.out.println("Invoked Pros.ProsList()");
         JSONArray response = new JSONArray();
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT ProID, ProName FROM Pros");
+            PreparedStatement ps = Main.db.prepareStatement("SELECT ProName, Position, AverageKills, AverageDeaths, AverageAssists, KDA FROM Pros");
             ResultSet results = ps.executeQuery();
-            while (results.next()==true) {
+            while (results.next() == true) {
                 JSONObject row = new JSONObject();
-                row.put("UserID", results.getInt(1));
-                row.put("UserName", results.getString(2));
+                row.put("ProName", results.getString(1));
+                row.put("Position", results.getString(2));
+                row.put("AverageKills", results.getFloat(3));
+                row.put("AverageDeaths", results.getFloat(4));
+                row.put("AverageAssists", results.getFloat(5));
+                row.put("KDA", results.getFloat(6));
                 response.add(row);
             }
             return response.toString();
@@ -30,4 +37,31 @@ public class Pros {
             return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
         }
     }
+
+    @GET
+    @Path("getPro/{ProName}") //getPro path specifies parameter ProName which relates to the input needed
+    public String getProName(@PathParam("ProName") String ProName) {
+        System.out.println("Invoked Pros.getPro()");
+        JSONArray response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT ProName, Position, AverageKills, AverageDeaths, AverageAssists, KDA FROM Pros WHERE ProName = ?"); //prepared statement which selects all of the fields in the Pros table given a ProName is inputted
+            ps.setString(1, ProName);
+            ResultSet results = ps.executeQuery();
+            while (results.next() == true) {
+                JSONObject row = new JSONObject(); //JSON object is formed form each data aspect sent
+                row.put("ProName", results.getString(1));
+                row.put("Position", results.getString(2));
+                row.put("AverageKills", results.getFloat(3));
+                row.put("AverageDeaths", results.getFloat(4));
+                row.put("AverageAssists", results.getFloat(5));
+                row.put("KDA", results.getFloat(6));
+                response.add(row);
+            }
+            return response.toString(); //sends a JSON object back to the JS code which consists of the data about the given ProName.
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage()); //error message if database error for debugging purposes
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+
 }
